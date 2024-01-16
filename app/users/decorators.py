83fedd0453086271afe59aeb1dbecd71e055e587
Михаил Горбань <91434795+usernameisavailablee@users.noreply.github.com>
2024-01-16 22:40:34 +0300

    commutate css and html

--- app/users/decorators.py
+++ app/users/decorators.py
@@ -1,33 +1,12 @@
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# from typing import Union
-#
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: Union[bool, None] = None
-#
-#
-# app = FastAPI()
-#
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-#
-# @app.get("/qq")
-# def test():
-#     return "hello"
-#
-#
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name": item.price, "item_id": item_id}
-
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 @app.get("/")
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import uvicorn
 from app.users.decorators import app
 
-uvicorn.run(app, host="127.0.0.1", port=8000)
+
+if __name__ == '__main__':
+     uvicorn.run("run:app", host="127.0.0.1", port=8000,reload=True)
